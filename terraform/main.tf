f3360384d5d08dde0ab96ddaf4c628f563b323f0
I562291 <I562291@fontysict.nl>
2026-09-08 21:14:17 +0000

    Add Terraform backend configuration for S3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket  = "terraform-state-bucket"
+    key = "terraform.tfstate"
+    region  = "eu-central-1"                      
+    encrypt = true
+  }
+}
+
 provider "aws" {
   region = "eu-central-1"
 }
