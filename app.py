8b50a02956d3414180389b7e74813f3af5dda87e
I562291 <I562291@fontysict.nl>
2026-09-15 19:42:28 +0000

    Add Flask application with ticket purchasing functionality and HTML templates

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/ticket")
+def ticket():
+    return render_template("ticket.html")
+
+@app.route("/confirmation")
+def confirmation():
+    return render_template("confirmation.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- terraform/main.tf
+++ terraform/main.tf
@@ -370,6 +370,48 @@ resource "aws_security_group" "monitoring_SG" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  tags = {
+    Project = "innovatech_solutions"
+    Name    = "monitoring_SG"
+  }
 }
 
 # Services
+# resource "aws_lb" "ALB" {
+#   name               = "ALB"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.ALB_SG.id]
+#   subnets = [
+#     aws_subnet.ALB_subnet1.id,
+#     aws_subnet.ALB_subnet2.id
+#   ]
+
+#   tags = {
+#     Project = "innovatech_solutions"
+#     Name    = "ALB"
+#   }
+# }
+
+# resource "aws_lb_target_group" "webserver_tg" {
+#   name        = "webserver_tg"
+#   target_type = "instance"
+#   port        = 80
+#   protocol    = "HTTP"
+#   vpc_id      = aws_vpc.vpc_innovatech_solutions.id
+# }
+
+# resource "aws_lb_listener" "http_listener" {
+#   load_balancer_arn = aws_lb.ALB.arn
+#   port              = 80
+#   protocol          = "HTTP"
+
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.webserver_tg.arn
+#   }
+# }
+
+resource "aws_" "name" {
+  
+}
