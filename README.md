e50d0c64f8f9ebe6a1d815fafa6ce73e9d516aea
I562291 <I562291@fontysict.nl>
2026-09-17 22:54:42 +0200

    dingen

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Innovatech-Solutions
-Case study about Innovatech Solutions
+Case study about Innovatech Solutions, a growing company that wants a modern cloud environment, focusing on scalability, expansion, resilience and efficiency.
--- terraform/main.tf
+++ terraform/main.tf
@@ -377,41 +377,165 @@ resource "aws_security_group" "monitoring_SG" {
 }
 
 # Services
-# resource "aws_lb" "ALB" {
-#   name               = "ALB"
-#   internal           = false
-#   load_balancer_type = "application"
-#   security_groups    = [aws_security_group.ALB_SG.id]
-#   subnets = [
-#     aws_subnet.ALB_subnet1.id,
-#     aws_subnet.ALB_subnet2.id
-#   ]
+# ALB ------------------------------------------------------------------------------------------------
+resource "aws_lb" "ALB" {
+  name               = "ALB"
+  internal           = false
+  load_balancer_type = "application"
 
-#   tags = {
-#     Project = "innovatech_solutions"
-#     Name    = "ALB"
-#   }
-# }
+  security_groups    = [aws_security_group.ALB_SG.id]
+  subnets = [
+    aws_subnet.ALB_subnet1.id,
+    aws_subnet.ALB_subnet2.id
+  ]
 
-# resource "aws_lb_target_group" "webserver_tg" {
-#   name        = "webserver_tg"
-#   target_type = "instance"
-#   port        = 80
-#   protocol    = "HTTP"
-#   vpc_id      = aws_vpc.vpc_innovatech_solutions.id
-# }
+  tags = {
+    Project = "innovatech_solutions"
+    Name    = "ALB"
+  }
+}
 
-# resource "aws_lb_listener" "http_listener" {
-#   load_balancer_arn = aws_lb.ALB.arn
-#   port              = 80
-#   protocol          = "HTTP"
-
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.webserver_tg.arn
-#   }
-# }
-
-resource "aws_" "name" {
+resource "aws_lb_target_group" "alb_webserver_tg" { # de ecs zal hier later aan toegevoegd worden om de webserver destinatie mee te geven
+  name        = "webserver-tg"
+  target_type = "ip" # ecs verzameld alle ip's en stuurt die door naar de alb
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.vpc_innovatech_solutions.id
   
+  tags = {
+    Project = "innovatech_solutions"
+  }
+}
+
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.ALB.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_webserver_tg.arn
+  }
+
+  tags = {
+    Project = "innovatech_solutions"
+    name = "alb_listener"
+  }
+
+}
+
+# --------------------------------------------------------------------------------------------------------
+
+# webserver ----------------------------------------------------------------------------------------------
+# 1. Haal automatisch de nieuwste ECS-Optimized AMI op voor T2 (Amazon Linux 2)
+data "aws_ssm_parameter" "ec2_ami" {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id" # ECS optimized AMI voor amazon linux 2
+}
+
+# 2. De blauwdruk voor je EC2 webservers
+resource "aws_launch_template" "ecs_webserver_template" {
+  name_prefix   = "ecs-webserver-"
+  image_id      = data.aws_ssm_parameter.ec2_ami.value
+  instance_type = "t2.micro" # t2.micro free tier. you can always expand ec2 during expansion.
+
+  network_interfaces {
+    associate_public_ip_address = false 
+    security_groups             = [aws_security_group.webserver_SG.id] 
+  }
+
+  # Dit linkt de EC2-computer aan je ECS-cluster.
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              echo ECS_CLUSTER=innovatech-cluster >> /etc/ecs/ecs.config
+              EOF
+  )
+}
+
+
+resource "aws_autoscaling_group" "webserver_asg" {
+  name                = "webserver-asg"
+  vpc_zone_identifier = [aws_subnet.Webserver_subnet1.id, aws_subnet.Webserver_subnet2.id]
+  desired_capacity    = 2
+  max_size            = 3
+  min_size            = 2
+
+  # Dit zorgt ervoor dat de Autoscaler ingrijpt als de ALB health check faalt
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
+
+  launch_template {
+    id      = aws_launch_template.ecs_webserver_template.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "AmazonECSManaged"
+    value               = ""
+    propagate_at_launch = true
+  }
+  tags = {
+    Project = "innovatech_solutions"
+    Name    = "webserver_asg"
+  }
+}
+
+# Het ECS Cluster (De manager van je containers) waar je ec2 instances in zitten.
+resource "aws_ecs_cluster" "innovatech_ecs_cluster" {
+  name = "innovatech-cluster"
+  
+  tags = {
+    Project = "innovatech_solutions"
+    Name    = "innovatech_ecs_cluster"
+  }
+}
+
+# Een container registry waar ik mijn image in opsla.
+resource "aws_ecr_repository" "container_registry" {
+  name                 = "container_registry"
+}
+
+# De Task Definition, instructies voor de ecs cluster voor een container (welke image?)
+resource "aws_ecs_task_definition" "webserver_task" {
+  family                   = "webserver-task"
+  network_mode             = "awsvpc" # awsvpc is nodig voor de alb target group omdat het de ip's van de containers doorstuurt naar de alb
+  requires_compatibilities = ["EC2"] # ec2 over fargate. fargate = serverless, maar duurder en minder control
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name      = "webserver"
+      # hier koppelen we de URL dynamisch aan de image die we in de ECR hebben gepusht
+      image     = "${aws_ecr_repository.container_registry.repository_url}:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
+
+# De ECS Service (Zorgt dat er altijd 2 containers draaien en koppelt aan de ALB)
+resource "aws_ecs_service" "webserver_service" {
+  name            = "webserver-service"
+  cluster         = aws_ecs_cluster.innovatech_ecs_cluster.id
+  task_definition = aws_ecs_task_definition.webserver_task.arn
+  desired_count   = 2
+  launch_type     = "EC2"
+
+  network_configuration {
+    subnets         = [aws_subnet.Webserver_subnet1.id, aws_subnet.Webserver_subnet2.id]
+    security_groups = [aws_security_group.webserver_SG.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.alb_webserver_tg.arn # koppelt de alb target group aan de ecs service zodat de alb weet waar hij de requests heen moet sturen
+    container_name   = "webserver"
+    container_port   = 80
+  }
+
+  depends_on = [aws_lb_listener.alb_listener]
 }
