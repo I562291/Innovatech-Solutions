39852ea008bd3faefbbe767d200ccd592ae306f3
I562291 <I562291@fontysict.nl>
2026-09-24 17:53:40 +0000

    Remove temporary ingress rule for local database access and set MySQL instance to not be publicly accessible

--- terraform/main.tf
+++ terraform/main.tf
@@ -306,14 +306,6 @@ resource "aws_security_group" "database_sg" {
     cidr_blocks = ["10.0.4.0/24"]
   }
 
-# tijdelijke test om database lokaal te kunnen gebruiken, dit moet later weg.
-  ingress { # git codespace -> Database
-  from_port   = 3306
-  to_port     = 3306
-  protocol    = "tcp"
-  cidr_blocks = ["4.180.183.246/32"]
-}
-
   egress { # Database -> Webserver
     from_port   = 3306
     to_port     = 3306
@@ -661,7 +653,7 @@ resource "aws_db_instance" "mysql" {
   skip_final_snapshot = true # zodat als ik terraform destroy/delete doe dat echt alles weg is, in productie is dit natuurlijk niet slim
   vpc_security_group_ids = [aws_security_group.database_sg.id]
   db_subnet_group_name = aws_db_subnet_group.mysql_subnet_group.name
-  publicly_accessible = true
+  publicly_accessible = false
   backup_retention_period = 7   # Kiest hoe lang de backups bewaard blijven (bijv. 7 dagen)
   backup_window           = "03:00-04:00" # Tijdstip in de nacht waarop de daily snapshot wordt gemaakt
 
